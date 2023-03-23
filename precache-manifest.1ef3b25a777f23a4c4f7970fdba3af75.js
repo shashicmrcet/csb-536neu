6dbf75321d87449f41c622d81fec2ac377a93a6c
Code Sandbox <hello@codesandbox.io>
2023-03-23 21:44:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1ef3b25a777f23a4c4f7970fdba3af75.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85944309c721959fa8d297bc24ea6d06",
+    "url": "https://shashicmrcet.github.io/csb-536neu/index.html"
+  },
+  {
+    "revision": "4957c991683e11a62007",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/css/main.f45ab72c.chunk.css"
+  },
+  {
+    "revision": "75d6a33915b48ccca76c",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/js/2.4b5249fe.chunk.js"
+  },
+  {
+    "revision": "cbc41762b08bce4213514217ce9f16f9",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/js/2.4b5249fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4957c991683e11a62007",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/js/main.c4cdce92.chunk.js"
+  },
+  {
+    "revision": "5c989d7ebf376a56317f",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/js/runtime-main.f4540b8f.js"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "ce08af25e34308083f7b019fef35ebd0",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/eduweb.ce08af25.png"
+  },
+  {
+    "revision": "10b0719343408095f5a9b369e0787115",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/firebase-icon.10b07193.svg"
+  },
+  {
+    "revision": "b005137d90d7a5dc2ac8cb12da2ad676",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/game.b005137d.png"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "38f9a4f29ee819486c821b193f592aed",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/logo-vercel.38f9a4f2.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "ecb5ec57f305689a6dc44a0828e4b893",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/netlify-icon.ecb5ec57.svg"
+  },
+  {
+    "revision": "d8fee7924d463334d60c5bf965237552",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/profile.d8fee792.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "a6b0c973650d817c0d8fe0c93a27e337",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/shopkart.a6b0c973.png"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "8647262c27c19c333a454f29066b979f",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/slide4.8647262c.webp"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/tailwindcss-icon.760a3ee4.svg"
+  },
+  {
+    "revision": "b655bc4c0c21798afd3c69584190e9c7",
+    "url": "https://shashicmrcet.github.io/csb-536neu/static/media/visioneco.b655bc4c.png"
+  }
+]);
